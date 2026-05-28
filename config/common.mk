91cd3100f7611b046cba4e934275dbabe94bd6cd
Anshuman X <anshuman190208@gmail.com>
2026-05-28 12:13:16 +0530

    Add ZenkaiOS common configuration settings

--- config/common.mk
+++ config/common.mk
@@ -1,1 +1,12 @@
+# ZenkaiOS Common Configuration
 
+PRODUCT_BRAND := ZenkaiOS
+PRODUCT_SYSTEM_NAME := ZenkaiOS
+PRODUCT_MANUFACTURER := ZenkaiOS
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    BuildDesc="ZenkaiOS"
+
+PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
+    ro.zenkai.version=1.0 \
+    ro.zenkai.codename=Ichi
